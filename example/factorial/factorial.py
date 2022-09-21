6c4b5cadd030cd815e6cc6587e660c3a9139d66d
yucing <e97456797979@gmail.com>
2022-09-21 15:29:15 +0800

    logFactorial

--- example/factorial/factorial.py
+++ example/factorial/factorial.py
@@ -1,8 +1,19 @@
+import math
+
 def factorial(n):
     r = 1
     for i in range(1, n+1):
         r *= i
     return r
 
+def logFactorial(n):
+    r = 0
+    for i in range(1, n+1):
+        r += math.log(i)
+    return r
+
 if __name__ == '__main__':
     print('factorial(10)=', factorial(10))
+    print('logFactorial(10)=', logFactorial(10))
+    print('exp(logFactorial(10))=', math.exp(logFactorial(10)))
+    assert round(math.exp(logFactorial(10))) == factorial(10)
--- example/factorial/test_factorial.py
+++ example/factorial/test_factorial.py
@@ -1,3 +1,5 @@
 from factorial import *
 def test_factorial():
     assert factorial(5) == 120
+def test_logFatorial():
+    assert factorial(5) == round(math.exp(logFactorial(5)))
