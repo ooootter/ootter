e7be31f979c6cfaf64a3b019d709ab19902b39ea
yucing <e97456797979@gmail.com>
2022-09-21 14:57:14 +0800

    add factorial

--- example/factorial/factorial.py
+++ example/factorial/factorial.py
@@ -2,4 +2,7 @@ def factorial(n):
     r = 1
     for i in range(1, n+1):
         r *= i
-     return r
+    return r
+
+if __name__ == '__main__':
+    print('factorial(10)=', factorial(10))
---
+++ example/factorial/test_factorial.py
@@ -0,0 +1,3 @@
+from factorial import *
+def test_factorial():
+    assert factorial(5) == 120
